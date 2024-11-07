4d31ced3530b2494d55b98fb58688c7380c0e5dc
Manoj <64075298+ManoJ-46@users.noreply.github.com>
2024-11-07 22:49:08 +0530

    Update jssateb_bot.py

--- jssateb_bot.py
+++ jssateb_bot.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 USER_TYPE, USERNAME, PASSWORD = range(3)
 
 # Updated token (replace with your bot token)
-TOKEN = ''
+TOKEN = '7391510808:AAHOPfdVOMg5d799Pi7Ig50VcSw5K5sRX1k'
 
 # CSV file name
 CSV_FILE = 'user_data_with_passwords.csv'
